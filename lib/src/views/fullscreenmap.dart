fb9d0659dfccb1fda8b7024a67f644225daa0885
pvictorf <41065334+pvictorf@users.noreply.github.com>
2020-08-27 17:38:30 -0300

    Melhorias na performance, fab location zoom fixed

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -13,15 +13,12 @@ class _FullScreenMapState extends State<FullScreenMap> {
   MapboxMapController mapController;
   MinMaxZoomPreference _minMaxZoomPreference = MinMaxZoomPreference.unbounded;
   CameraTargetBounds _cameraTargetBounds = CameraTargetBounds.unbounded;
-  List<Object> _featureQueryFilter;
-  bool _isMoving = false;
   bool _scrollGesturesEnabled = true;
   bool _tiltGesturesEnabled = true;
   bool _zoomGesturesEnabled = true;
   bool _myLocationEnabled = true;
   bool _rotateGesturesEnabled = true;
   bool _compassEnabled = true;
-  bool _trackCameraPosition = true;
   LatLng _currentLocation = LatLng(-22.4891277, -43.4798553);
   LatLng _userPosition;
 
@@ -41,11 +38,11 @@ class _FullScreenMapState extends State<FullScreenMap> {
     }
   }
 
-  void _moveCameraToUser(LatLng latLng, [double zoom = 14.0]) {
+  void _moveCameraToUser(LatLng latLng) {
     if (mapController != null) {
-      mapController.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(target: latLng, zoom: zoom),
-      ));
+      mapController.animateCamera(
+        CameraUpdate.newLatLng(latLng)
+      );
     }
   }
 
@@ -61,8 +58,7 @@ class _FullScreenMapState extends State<FullScreenMap> {
       body: Container(
         child: MapboxMap(
           onMapCreated: _onMapCreated,
-          initialCameraPosition:
-              CameraPosition(target: _currentLocation, zoom: 15),
+          initialCameraPosition: CameraPosition(target: _currentLocation, zoom: 14),
           myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
           myLocationRenderMode: MyLocationRenderMode.GPS,
           myLocationEnabled: _myLocationEnabled,
@@ -71,19 +67,9 @@ class _FullScreenMapState extends State<FullScreenMap> {
           scrollGesturesEnabled: _scrollGesturesEnabled,
           rotateGesturesEnabled: _rotateGesturesEnabled,
           minMaxZoomPreference: _minMaxZoomPreference,
-          trackCameraPosition: _trackCameraPosition,
           compassEnabled: _compassEnabled,
           cameraTargetBounds: _cameraTargetBounds,
-          onMapClick: (point, latLng) async {
-            print(
-                "Map click: ${point.x},${point.y}   ${latLng.latitude}/${latLng.longitude}");
-            print("Filter $_featureQueryFilter");
-            List features = await mapController.queryRenderedFeatures(
-                point, [], _featureQueryFilter);
-            if (features.length > 0) {
-              print(features[0]);
-            }
-          },
+          onMapClick: (point, latLng) async {},
         ),
       ),
       floatingActionButton: FloatingActionButton(
