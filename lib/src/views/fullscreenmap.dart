549c6ad264d0bf326789786a86b7ff17d2fac693
pvictorf <41065334+pvictorf@users.noreply.github.com>
2020-08-29 15:20:34 -0300

    Added directions and user location fab

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:location/location.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class FullScreenMap extends StatefulWidget {
   const FullScreenMap({Key key}) : super(key: key);
@@ -12,6 +14,7 @@ class FullScreenMap extends StatefulWidget {
 class _FullScreenMapState extends State<FullScreenMap> {
   MapboxMapController mapController;
   String _mapStyle = "mapbox://styles/mapbox/streets-v11";
+  String _access_token = "YOUR_ACCESS_TOKEN";
   MinMaxZoomPreference _minMaxZoomPreference = MinMaxZoomPreference.unbounded;
   CameraTargetBounds _cameraTargetBounds = CameraTargetBounds.unbounded;
   bool _scrollGesturesEnabled = true;
@@ -23,6 +26,7 @@ class _FullScreenMapState extends State<FullScreenMap> {
   bool _trackCameraPosition = true;
   LatLng _currentLocation = LatLng(-22.4891277, -43.4798553);
   LatLng _userPosition;
+  String _userCoords;
 
   @override
   void initState() {
@@ -42,6 +46,7 @@ class _FullScreenMapState extends State<FullScreenMap> {
       var locationData = await location.getLocation();
       setState(() {
         _userPosition = LatLng(locationData.latitude, locationData.longitude);
+        _userCoords = "${locationData.longitude},${locationData.latitude}";
       });
     }
   }
@@ -56,28 +61,89 @@ class _FullScreenMapState extends State<FullScreenMap> {
     mapController = controller;
     mapController.setTelemetryEnabled(false);
     mapController.invalidateAmbientCache();
+    mapController.clearLines();
+  }
+
+  void _add() async {
+    String startCoords = _userCoords;
+    String endCoords = "-43.119890,-22.896176";
+    String geometries = "geojson";
+    List<dynamic> coordinates = [];
+    List<LatLng> _geometry = [];
+
+    String url =
+        "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/$startCoords;$endCoords?alternatives=true&geometries=$geometries&steps=true&access_token=$_access_token";
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var json = convert.jsonDecode(response.body);
+
+      coordinates = json["routes"][0]["geometry"]["coordinates"];
+
+      coordinates
+              .forEach((coords) => _geometry.add(LatLng(coords[1], coords[0])));
+
+      await mapController.clearLines();
+      await mapController.addLine(
+        LineOptions(
+            geometry: _geometry,
+            lineColor: "#458de1",
+            lineWidth: 9.4,
+            lineJoin: "LINE_JOIN_ROUND",
+            lineOpacity: 1.0,
+            draggable: true),
+      );
+
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: MapboxMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition:
-            CameraPosition(target: _currentLocation, zoom: 14),
-        myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
-        myLocationRenderMode: MyLocationRenderMode.GPS,
-        myLocationEnabled: _myLocationEnabled,
-        zoomGesturesEnabled: _zoomGesturesEnabled,
-        tiltGesturesEnabled: _tiltGesturesEnabled,
-        scrollGesturesEnabled: _scrollGesturesEnabled,
-        rotateGesturesEnabled: _rotateGesturesEnabled,
-        minMaxZoomPreference: _minMaxZoomPreference,
-        compassEnabled: _compassEnabled,
-        cameraTargetBounds: _cameraTargetBounds,
-        trackCameraPosition: _trackCameraPosition,
-        styleString: _mapStyle,
-      ),
+    return Stack(
+      children: [
+        MapboxMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition:
+              CameraPosition(target: _currentLocation, zoom: 14),
+          myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
+          myLocationRenderMode: MyLocationRenderMode.GPS,
+          myLocationEnabled: _myLocationEnabled,
+          zoomGesturesEnabled: _zoomGesturesEnabled,
+          tiltGesturesEnabled: _tiltGesturesEnabled,
+          scrollGesturesEnabled: _scrollGesturesEnabled,
+          rotateGesturesEnabled: _rotateGesturesEnabled,
+          minMaxZoomPreference: _minMaxZoomPreference,
+          compassEnabled: _compassEnabled,
+          cameraTargetBounds: _cameraTargetBounds,
+          trackCameraPosition: _trackCameraPosition,
+          styleString: _mapStyle,
+        ),
+        Container(
+            alignment: Alignment.bottomRight,
+            margin: EdgeInsets.only(bottom: 80, right: 10),
+            child: FloatingActionButton(
+              elevation: 2,
+              backgroundColor: Color(0xff458de1),
+              child: Icon(Icons.line_style),
+              onPressed: () {
+                _add();
+              },
+            )),
+        Container(
+            alignment: Alignment.bottomRight,
+            padding: EdgeInsets.all(10),
+            child: FloatingActionButton(
+              elevation: 2,
+              backgroundColor: Color(0xff458de1),
+              child: Icon(Icons.gps_fixed),
+              onPressed: () {
+                _moveCameraToUser(_userPosition);
+              },
+            )),
+      ],
     );
   }
 }
